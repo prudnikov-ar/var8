0a1ed346fa51291666017409375489444248bac6
Vlad Yastrebov <leonorino@icloud.com>
2023-12-06 21:47:59 +0700

    CLI to print syracuse sequence

---
+++ main.py
@@ -0,0 +1,40 @@
+def syracuse_sequence(n: int) -> list[int]:
+    sequence = [n]
+
+    while n != 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n *= 3
+            n += 1
+        sequence.append(n)
+
+    return sequence
+
+
+def syracuse_max(n: int) -> int:
+    sequence = syracuse_sequence(n)
+
+    max_element = 1
+    for element in sequence:
+        if element > max_element:
+            max_element = element
+
+    return max_element
+
+
+if __name__ == '__main__':
+    while True:
+        line = input('Enter a syracuse sequence starting number: ')
+
+        try:
+            n = int(line)
+        except ValueError:
+            print('Incorrect input!')
+            continue
+
+        sequence = syracuse_sequence(n)
+        max_element = syracuse_max(n)
+        print('Sequence:', *sequence)
+        print(f'Max element: {max_element}')
+        break
